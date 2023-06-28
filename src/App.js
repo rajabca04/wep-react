8dd64b041fd849eda55970bf648ee2efc37e9de0
rajabca04 <rajabca201804@gmail.com>
2023-06-28 22:09:06 +0530

    this is my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+ export var App = ()=> {
+  return(
+  
+    <div >
+      <h1 className="text-amber-400">Hello world..!</h1>
+    </div>
+  )
+}
+export var Test1 = ()=>{
+  return(
+    <div>
+      <h3>This is Test1</h3>
+    </div>
+  )
+}
---
+++ src/Test.js
@@ -0,0 +1,16 @@
+
+
+let Test = () => {
+    return (
+        <div class="flex items-center max-w-md max-h-xs bg-blue-200 text-white">
+            <img src="https://media.licdn.com/dms/image/D4D03AQFW3Jzq3tukEg/profile-displayphoto-shrink_400_400/0/1682164118560?e=1693440000&v=beta&t=SFBwfLNdWT3RPidEiUm_XtjrmgRFHg0Ux3PeSviRQSc" alt="img"    className="self-center w-10 h-10 rounded-full mx-1" />
+            <div>
+                <strong>Andrew Alfred</strong>
+                <span>Technical advisor</span>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Test;
